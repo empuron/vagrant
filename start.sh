fcbc56cf278de46b86bb7f5cfc4ddd3a5fe31cbc
empuron <moritz.mueller@empuron.de>
2016-07-10 01:58:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 echo "--- Start Script ---"
 useradd empuron
 usermod -a -G wheel empuron
-yum install epel-release -y 2>/dev/null
-yum install -y salt-minion 2>/dev/null
+yum install epel-release -y >/dev/null
+yum install -y salt-minion >/dev/null
 mkdir /srv/salt
 chmod 777 /srv/salt
 service salt-minion start
