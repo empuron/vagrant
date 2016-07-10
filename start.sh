550b01feb6e01a7ebcd5c065d8978c8d64d70f52
empuron <moritz.mueller@empuron.de>
2016-07-10 11:59:29 +0200

    Kein Ausgabe anzeigen

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "--- Start Script ---"
-useradd empuron
-usermod -a -G wheel empuron
+useradd empuron >/dev/null
+usermod -a -G wheel empuron >/dev/null
 yum install epel-release -y >/dev/null
 yum install -y salt-minion >/dev/null
-mkdir /srv/salt
-chmod 777 /srv/salt
-service salt-minion start
+mkdir /srv/salt >/dev/null
+chmod 777 /srv/salt >/dev/null
+service salt-minion start >/dev/null
 echo "--- End Script ---"
